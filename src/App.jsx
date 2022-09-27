8c02515271bb2db858abb75417731f198b25c131
nikko <nikkorivas@outlook.com>
2022-09-27 16:44:47 +0100

    added location working with api

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import "./App.css";
 
 function App() {
   const apikey = process.env.REACT_APP_API_KEY;
-  console.log(apikey);
+  navigator.geolocation.getCurrentPosition((loc) => {
+    // console.log(loc.coords.latitude, loc.coords.longitude);
+    localStorage.setItem("locationDataLong", loc.coords.longitude);
+    localStorage.setItem("locationDataLat", loc.coords.latitude);
+  });
+  const locationDataLong = localStorage.getItem("locationDataLong");
+  const locationDataLat = localStorage.getItem("locationDataLat");
+  console.log(locationDataLong, locationDataLat);
+
   const getWeatherData = async () => {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=51.4249,-0.1032`
+      `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${locationDataLat},${locationDataLong}`
     );
     let weatherDataJson = await response.json();
     console.log(weatherDataJson);
   };
   getWeatherData();
-  navigator.geolocation.getCurrentPosition((loc) => {
-    console.log(loc.coords.latitude, loc.coords.longitude);
-  });
+
   return (
     <div className="App">
       <h1>TEST</h1>
