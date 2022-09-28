a3e3f3ba0c54a53ca7a783eafb916d83f2c3a000
nikko <nikkorivas@outlook.com>
2022-09-28 14:06:57 +0100

    added greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,15 @@ import { useEffect, useState } from "react";
 function App() {
   const [weatherData, setWeatherData] = useState();
 
-  const apikey = process.env.REACT_APP_API_KEY;
+  const currentHour = new Date().getHours();
+  let greetingTime = "Morning!";
+  if (currentHour >= 12) {
+    greetingTime = "Afternoon!";
+  }
+  if (currentHour >= 18) {
+    greetingTime = "Evening!";
+  }
+
   navigator.geolocation.getCurrentPosition((loc) => {
     // console.log(loc.coords.latitude, loc.coords.longitude);
     localStorage.setItem("locationDataLong", loc.coords.longitude);
@@ -18,30 +26,39 @@ function App() {
   const locationDataLat = localStorage.getItem("locationDataLat");
   // console.log(locationDataLong, locationDataLat);
 
+  const apikey = process.env.REACT_APP_API_KEY;
   const getWeatherData = async () => {
     const response = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${locationDataLat},${locationDataLong}`
     );
     const weatherDataJson = await response.json();
     setWeatherData(weatherDataJson);
+    console.log(weatherDataJson);
     // return weatherDataJson;
   };
   // console.log(weatherData);
   // console.log(weatherData?.location);
+
+  const welcomeTts = () => {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = `Good ${greetingTime}. The temperature is ${weatherData?.current.temp_c}°C. The weather is ${weatherData?.current.condition.text}, with wind speeds of ${weatherData?.current.wind_mph} mph`;
+    window.speechSynthesis.speak(msg);
+    console.log("tts");
+  };
   useEffect(() => {
     getWeatherData();
-    speechSynthesis.speak(
-      new SpeechSynthesisUtterance(
-        `Good Morning. The temperature is ${weatherData?.current.temp_c}°C. The weather is ${weatherData?.current.condition.text}, with wind speeds of ${weatherData?.current.wind_mph} mph`
-      )
-    );
+
+    window.onload = function () {
+      console.log("refresh");
+      setTimeout(welcomeTts(), 3000);
+    };
   }, []);
 
   return (
     <div className="App">
       {/* <h1>weather: {weatherData?.location.name}</h1> */}
       <div id="greeting-widget" className="widget widget--super-wide">
-        <h3>Good Morning</h3>
+        <h3>Good {greetingTime}</h3>
       </div>
       <div id="location-widget" className="widget widget--wide widget__row">
         <div>
@@ -50,7 +67,7 @@ function App() {
           <h1>{weatherData?.location.region}</h1>
           <h1>{weatherData?.location.name}</h1>
         </div>
-        <img src={compassIcon}></img>
+        <img src={compassIcon} alt="compass icon"></img>
       </div>
       <div id="temperature-widget" className="widget">
         <h2>Temperature</h2>
@@ -59,7 +76,7 @@ function App() {
             <h3>{weatherData?.current.temp_c}°C</h3>
             <h3>{weatherData?.current.temp_f}°F</h3>
           </div>
-          <img src={thermometerIcon}></img>
+          <img src={thermometerIcon} alt="thermometer icon"></img>
         </div>
       </div>
       <div id="weather-widget" className="widget">
@@ -70,12 +87,12 @@ function App() {
         <h2>Wind Speed</h2>
         <h3>{weatherData?.current.wind_mph} mph</h3>
         <h3>{weatherData?.current.wind_kph} kph</h3>
-        <img src={windIcon}></img>
+        <img src={windIcon} alt="wind icon"></img>
       </div>
       <div id="winddirection-widget" className="widget">
         <h2>Wind Direction</h2>
         <h3>{weatherData?.current.wind_dir}</h3>
-        <img src={windDirectionIcon}></img>
+        <img src={windDirectionIcon} alt="wind direction icon"></img>
       </div>
     </div>
   );
